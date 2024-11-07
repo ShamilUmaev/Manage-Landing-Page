e0098750f8e245d89cf243b5943cbddd09264d87
Shamil Umaev <shamil.umaev@hotmail.com>
2024-11-07 23:44:47 +0100

    Add testimonial swiper using Swiper.js

---
+++ index.js
@@ -0,0 +1,18 @@
+const swiper = new Swiper('.swiper', {
+
+    direction: 'horizontal',
+    loop: true,
+  
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+
+    speed: 5000
+  });
+  
