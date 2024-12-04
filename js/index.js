17cafcffe7b688eb114fa904dd2ba176740a313c
Shamil Umaev <shamil.umaev@hotmail.com>
2024-12-04 22:33:45 +0100

    Add breakpoints for larger devices and make desktop navigation sticky

--- js/index.js
+++ js/index.js
@@ -32,6 +32,9 @@ const closeBtn = document.querySelector('.close-mobile-menu-btn');
 const mobileNav = document.querySelector('.main-nav-list');
 const pageContainer = document.querySelector('.page-outer-container');
 const overlay = document.querySelector('.page-outer-container');
+const headerTop = document.querySelector('.header-top');
+const desktopNav = document.querySelector('.main-nav');
+const breakpoint = window.matchMedia("(min-width: 992px)");
 
 const showMobileNav = function () {
     hamburgerBtn.classList.remove('visible');
@@ -41,6 +44,7 @@ const showMobileNav = function () {
     mobileNav.classList.remove('hide-mobile-menu');
     mobileNav.classList.add('show-mobile-menu');
     pageContainer.classList.add('overlay');
+    // headerTop.style.backgroundColor = "transparent";
 }
 
 const hideMobileNav = function () {
@@ -51,11 +55,27 @@ const hideMobileNav = function () {
     mobileNav.classList.remove('show-mobile-menu');
     mobileNav.classList.add('hide-mobile-menu');
     pageContainer.classList.remove('overlay');
+    // headerTop.style.backgroundColor = "#fff";
+}
+
+const stickHeaderToTop = function () {
+  if(window.scrollY > 20) {
+    headerTop.classList.add('sticky-header');
+    desktopNav.style.padding = "2.5rem 0";
+  } else {
+    headerTop.classList.remove('sticky-header');
+    desktopNav.style.padding = "5rem 0 0 0";
+  }
 }
 
 const init = function () {
   hamburgerBtn.addEventListener('click', showMobileNav);
   closeBtn.addEventListener('click', hideMobileNav);
+  window.addEventListener('scroll', () => {
+    if(breakpoint.matches) {
+      stickHeaderToTop();
+    }
+  });
   document.body.addEventListener('click', (e) => {
     if(e.target.classList.contains('overlay')) {
       hideMobileNav();
